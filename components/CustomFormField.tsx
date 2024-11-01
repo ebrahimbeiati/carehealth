0508c79c85dff91886a8bd47c6f6b436c89e8986
Ebrahim Beiaty <ebrahimbeiaty@gmail.com>
2024-11-01 00:27:57 +0000

    appointment

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,82 @@
+"use client";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+import Image from "next/image";
+
+interface CustomProps {
+  control: Control<any>,
+  name: string,
+  label?: string,
+  fieldType: FormFieldType,
+  placeholder?: string,
+  iconSrc?: string,
+  iconAlt?: string,
+  disabled?: boolean,
+  dateFormat?: string,
+  showTimeSelect?: boolean,
+  children?: React.ReactNode,
+  renderSkeleton?: (field: any) => React.ReactNode,
+}
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps}) => {
+  const {fieldType, placeholder, iconSrc, iconAlt, disabled, dateFormat, showTimeSelect} = props;
+  switch (fieldType) {
+    case FormFieldType.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400 ">
+          {iconSrc && (
+            <Image
+              className="ml-2 rounded-full"
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt ?? 'icon'}
+            />
+          )}
+          <FormControl>
+            <Input
+              {...field}
+              placeholder={placeholder}
+              className="shad-input border-0 bg-slate-200"
+            />
+          </FormControl>
+        
+        </div>
+       
+      );
+    default:
+      break;
+  }
+};
+
+const CustomFormField = (props: CustomProps) => {
+  const {control, fieldType, name, label} = props;
+  
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldType.CHECKBOX && label &&(
+            <FormLabel >{label}</FormLabel>
+          )}
+        < RenderField field={field} props={props} />
+
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
